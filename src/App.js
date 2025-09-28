aac3edf2e491bbfe2d06b8d2604b37d9c1b3e30e
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-09-28 23:27:11 +0900

    feat: state 변수 사용

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
+/* eslint-disable */
+
 import logo from "./logo.svg";
 import "./App.css";
+import { use, useState } from "react";
 
 function App() {
+
   let post = "강남 우동 맛집";
+  let [글제목, b] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [따봉, 따봉변경] = useState(0);
+
+  function 함수() {
+    c(따봉 + 1);
+    console.log(따봉);
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>블로그</h4>
+        <h2>React Blog</h2>
       </div>
-      <h4>{post}</h4>
+      <div className="list">
+        <h4>{글제목[0]}<span>좋아요</span> {따봉} </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}<span onClick={() => { 따봉변경(따봉 + 1) }}>👍️</span> {따봉} </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}<span>좋아요</span> {따봉} </h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
