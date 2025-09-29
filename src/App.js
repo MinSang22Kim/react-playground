49b6aabc8e246b6fdf78edceb9cc89c9383014a4
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-09-29 12:35:10 +0900

    feat: 버튼 구현

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ import "./App.css";
 import { use, useState } from "react";
 
 function App() {
-
   let post = "강남 우동 맛집";
-  let [글제목, b] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
 
   function 함수() {
@@ -20,8 +19,20 @@ function App() {
       <div className="black-nav">
         <h2>React Blog</h2>
       </div>
+
+      <button onClick={() => {
+        let copy = [...글제목];
+        글제목변경(copy.sort());
+      }}>가나다순 정렬</button>
+
+      <button onClick={() => {
+        let copy = [...글제목];
+        copy[0] = '여자 코트 추천';
+        글제목변경(copy);
+      }}>글수정</button>
+
       <div className="list">
-        <h4>{글제목[0]}<span>좋아요</span> {따봉} </h4>
+        <h4>{글제목[0]}<span> 좋아요</span> {따봉} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
