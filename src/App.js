23b61cd083177f25275c94b9b1ce118a64c27548
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-09-29 12:44:54 +0900

    feat: 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import { use, useState } from "react";
 
 function App() {
+
   let post = "강남 우동 맛집";
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
@@ -43,8 +44,20 @@ function App() {
         <h4>{글제목[2]}<span>좋아요</span> {따봉} </h4>
         <p>2월 17일 발행</p>
       </div>
+
+      <Modal></Modal>
     </div>
   );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
